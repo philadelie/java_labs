85c53526db0ea13c95644b244544f3688cd93053
philadelie <neomalinka@gmail.com>
2024-02-16 16:30:44 +0100

    debugged

--- src/main/java/ntudp/psj/lab2/Matrix.java
+++ src/main/java/ntudp/psj/lab2/Matrix.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Matrix {
-    static final int MIN_MATRIX_POINT = -100;
+    static final int MIN_MATRIX_POINT = 0;
     static final int MAX_MATRIX_POINT = 100;
     static final int MAX_MATRIX_SIZE = 20;
     static Scanner sc = new Scanner(System.in);
@@ -19,11 +19,11 @@ public class Matrix {
         if (isRandomMatrix) generateRandomMatrix(matrix, rows, cols);
         else inputMatrixValues(matrix, rows, cols);
 
-        printMatrix(matrix, rows, cols);
+        printMatrix(matrix, rows);
         System.out.println("Min: " + findMatrixMin(matrix, rows, cols));
         System.out.println("Max: " + findMatrixMax(matrix, rows, cols));
-        System.out.println("Arithmetic mean: " + calculateMatrixArithmeticMean(matrix, rows, cols));
-        System.out.println("Geometric mean: " + calculateMatrixGeometricMean(matrix, rows, cols));
+        System.out.printf("Arithmetic mean: %.2f%n", calculateMatrixArithmeticMean(matrix, rows, cols));
+        System.out.printf("Geometric mean: %.2f%n", calculateMatrixGeometricMean(matrix, rows, cols));
     }
 
     private static boolean askIfRandomMatrix() {
@@ -35,17 +35,25 @@ public class Matrix {
             String answer = sc.next().toLowerCase();
 
             if (Arrays.asList(yesAnswers).contains(answer)) return true;
-            else if (Arrays.asList(noAnswers).contains(answer)) return false;
-            else System.out.println("Don't understand. Try again.");
+            if (Arrays.asList(noAnswers).contains(answer)) return false;
+            System.out.println("Don't understand. Try again.");
         }
     }
 
     private static int defineMatrixSize(String dimension) {
+        String str = String.format("Enter number of %s [<=%d]: ", dimension, MAX_MATRIX_SIZE);
+        return checkNumericInput(str, 0, MAX_MATRIX_SIZE);
+    }
+
+    private static int checkNumericInput(String outputMessage, int min, int max) {
         while (true) {
-            System.out.printf("Enter the number of %s: ", dimension);
-            int size = sc.nextInt();
-            if (size > 0 && size <= MAX_MATRIX_SIZE) return size;
-            else System.out.println("Invalid input. Please enter a number between 1 and " + MAX_MATRIX_SIZE);
+            System.out.print(outputMessage);
+            if (sc.hasNextInt()) {
+                int x = sc.nextInt();
+                if (x > min && x <= max) return x;
+                else continue;
+            }
+            sc.next();
         }
     }
 
@@ -59,37 +67,27 @@ public class Matrix {
     }
 
     private static void inputMatrixValues(int[][] arr, int rows, int cols) {
-        System.out.println("Now, enter the matrix' elements values [-100; 100].");
+        System.out.printf("Now, enter the matrix' elements values [%d; %d].%n", MIN_MATRIX_POINT, MAX_MATRIX_POINT);
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                while (true) {
-                    System.out.printf("%d,%d = ", i + 1, j + 1);
-                    if (sc.hasNextInt()) {
-                        int x = sc.nextInt();
-                        if (x >= MIN_MATRIX_POINT && x <= MAX_MATRIX_POINT) {
-                            arr[i][j] = x;
-                            break;
-                        }
-                    }
-                }
+                String str = String.format("[%d,%d] = ", i + 1, j + 1);
+                arr[i][j] = checkNumericInput(str, MIN_MATRIX_POINT, MAX_MATRIX_POINT);
             }
         }
     }
 
-    private static void printMatrix(int[][] arr, int rows, int cols) {
-        System.out.println("Matrix:");
+    private static void printMatrix(int[][] arr, int rows) {
+        System.out.printf("%nMatrix:%n");
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.printf("%d ", arr[i][j]);
-            }
-            System.out.println();
+            System.out.println(Arrays.toString(arr[i]));
         }
+        System.out.println();
     }
 
     private static int findMatrixMin(int[][] arr, int rows, int cols) {
         int min = arr[0][0];
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (arr[i][j] < min) min = arr[i][j];
             }
         }
@@ -98,31 +96,28 @@ public class Matrix {
 
     private static int findMatrixMax(int[][] arr, int rows, int cols) {
         int max = arr[0][0];
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (arr[i][j] > max) max = arr[i][j];
             }
         }
         return max;
     }
 
-    private static float calculateMatrixArithmeticMean(int[][] arr, int rows, int cols) {
-        int sum = 0;
+    private static double calculateMatrixArithmeticMean(int[][] arr, int rows, int cols) {
+        double sum = 0.0;
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+            for (int j = 0; j < cols; j++)
                 sum += arr[i][j];
-            }
         }
-        return (float)sum / (rows * cols);
+        return sum / (rows * cols);
     }
 
-    private static float calculateMatrixGeometricMean(int[][] arr, int rows, int cols) {
-        int product = 1;
+    private static double calculateMatrixGeometricMean(int[][] arr, int rows, int cols) {int product = 1;
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+            for (int j = 0; j < cols; j++)
                 product *= arr[i][j];
-            }
         }
-        return (float)Math.pow(product, 1.0 / (rows * cols));
+        return Math.pow(product, 1.0 / (rows * cols));
     }
 }
